4a2ceb962950be922ba0194fe654592e884d2aca
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2021-07-18 21:54:53 +0700

    Add files via upload

--- js/getdata.js
+++ js/getdata.js
@@ -94,7 +94,7 @@ function FindID(gLineID) {
 	    if (!doc.empty) {
 	    	//alert("มีข้อมูลอยู่แล้ว");
 	        //alert(sessionStorage.getItem("EmpName")+"-----");
-			alert(sessionStorage.getItem("EmpName")+"----"+gLineID+"kkkk");
+			alert(sessionStorage.getItem("EmpName")+"----"+gLineID+"7777");
 	    	document.getElementById('gotopage').style.display='block';
 	        //console.log("Document data:", doc[0].data());
 	    } else {
